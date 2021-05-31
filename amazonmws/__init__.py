9eb672a06dac888830e1db594c8c86e209175783
cpburnz <cpburnz@gmail.com>
2021-05-30 20:55:30 -0400

    Release v1.0.3

--- amazonmws/__init__.py
+++ amazonmws/__init__.py
@@ -5,11 +5,11 @@ API.
 """
 
 __authors__ = ["Caleb P. Burns", "Wes Hansen"]
-__copyright__ = u"Copyright © 2012-2013 Caleb P. Burns, Wes Hansen"
+__copyright__ = u"Copyright © 2012-2021 Caleb P. Burns, Wes Hansen"
 __created__ = "2012-11-30"
 __credits__ = ["Joshua D. Burns"]
 __license__ = "MPL 2.0"
-__modified__ = "2018-05-17"
+__modified__ = "2021-05-30"
 __project__ = "amazonmws"
 __status__ = "Production"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
